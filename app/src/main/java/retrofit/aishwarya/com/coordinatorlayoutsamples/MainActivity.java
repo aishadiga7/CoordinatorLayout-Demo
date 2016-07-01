9d60cb4e130daeb894c153a8ee081df1d9ba14a1
aishwarya <aishwarya@robosoftin.com>
2016-07-01 14:34:37 +0530

    commiting initial code

---
+++ app/src/main/java/retrofit/aishwarya/com/coordinatorlayoutsamples/MainActivity.java
@@ -0,0 +1,18 @@
+package retrofit.aishwarya.com.coordinatorlayoutsamples;
+
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("Collapsing");
+        collapsingToolbarLayout.setExpandedTitleColor(ContextCompat.getColor(this, android.R.color.transparent));
+    }
+}
